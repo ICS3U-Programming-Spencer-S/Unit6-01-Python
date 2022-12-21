68be5a92ea5649773a13d4d0d1d0b9e5bea20274
Spencer Scarlett <Spencer.Scarlett@stu.ocsb.ca>
2022-12-21 21:51:00 +0000

    Fixed average_calc.py

--- average_calc.py
+++ average_calc.py
@@ -2,7 +2,7 @@
 
 # Created By: Spencer Scarlett
 # Date: Dec 19, 2022
-# A program which uses lists to generate 10 numbers 
+# A program which uses lists to generate 10 numbers
 # with a range of 1/100 and find the average
 
 
@@ -13,29 +13,30 @@ def main():
 
     # intro message
     print("This is a program that generates 10 numbers and finds the average")
-    print ("")
+    print("")
 
-    # creates an empty list and sum  
+    # creates an empty list and sum
     list_num = []
     sum = 0
 
     # for loop in range of 10 as it needs 10 numbers
-    for counter in range (10):
-        #creates a number from 0-100
+    for counter in range(10):
+        # creates a number from 0-100
         random_num = random.randint(0, 100)
         # displays number was added to list
-        print (f"{random_num} was added to the list")
+        print(f"{random_num} was added to the list")
         # appends the number to the list
         list_num.append(random_num)
 
     # for loop in range of 10 that calculates the sum of the past 10 numbers
-    for counter in range (10):
+    for counter in range(10):
         sum += list_num[counter]
 
     average = sum / 10
     # output message
-    print ("")
-    print (f"The average of {list_num} is {average}")
+    print("")
+    print(f"The average of {list_num} is {average}")
+
 
 if __name__ == "__main__":
     main()
